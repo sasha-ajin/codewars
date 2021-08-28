78c9e6fe2daef15aa8556487073498a1cd2d6213
Sasha Ajintarev <77781886+sasha-ajin@users.noreply.github.com>
2021-08-28 19:38:18 +0300

    Delete 7kyu_sum_of_digits.py

--- 7kyu_sum_of_digits.py
+++
@@ -1,9 +0,0 @@
-"""
-It involves implementing a program that sums the digits of a non-negative integer. For example, the sum of 3433 digits
-is 13.
-"""
-def sum_of_digits(digits):
-    sum = int()
-    for number in digits:
-        sum += int(number)
-    return sum
